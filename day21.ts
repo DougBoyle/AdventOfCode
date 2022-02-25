ff7fad33ee48f35262f8f44ec60c57506739c1ce
Douglas Boyle <douglas.boyle@softwire.com>
2022-02-25 17:32:59 +0000

    Day 21 complete

--- day21.ts
+++ day21.ts
@@ -39,22 +39,42 @@ const getLine = (function () {
     return async () => ((await getLineGen.next()).value);
 })();
 
-let lastLine = "";
-async function getUserInput(): Promise<bigint> {
-    if (lastLine.length === 0) {
-        lastLine = (await getLine()) + "\n";
-    } 
-    const result = lastLine.charCodeAt(0);
-    lastLine = lastLine.substring(1);
-    return BigInt(result);
-}
-
-const script = `OR A T
+// Jumps 3 sqaures, so check there is something that needs jumping over, and something on 4th square to land on
+const scriptPart1 = `OR A T
 AND B T
 AND C T
 NOT T J
 AND D J
+WALK
 `;
+
+// Additionally, don't jump prematurely when 5th square empty (so would immediately jump again) and 8th also empty (nowhere to land)
+// i.e. !(A . B . C) . D . !(!E . !H) 
+// = (!A + !B + !C) . D . (E + H)
+const scriptPart2 = `OR A T
+AND B T
+AND C T
+OR E J
+OR H J
+AND D J
+NOT T T
+AND T J
+RUN
+`;
+
+let isPreSetInput = true;
+let lastLine = scriptPart2;
+async function getUserInput(): Promise<bigint> {
+    if (lastLine.length === 0) {
+        isPreSetInput = false;
+        lastLine = (await getLine()) + "\n";
+    } 
+    const result = lastLine.charCodeAt(0);
+    if (isPreSetInput) process.stdout.write(lastLine.charAt(0));
+    lastLine = lastLine.substring(1);
+    return BigInt(result);
+}
+
 
 /*
 SpringScript
